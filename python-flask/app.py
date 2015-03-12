18a53ce67cfb67fb84e5618dbb7ec9bf32ceda12
Doru Arfire <doruarfire@gmail.com>
2015-03-12 23:58:23 +0200

    Using only callbacks, no polling required.

--- python-flask/app.py
+++ python-flask/app.py
@@ -22,66 +22,38 @@ def index():
 
 
 # receives a product url and triggers a purchase for that product
-@app.route('/purchase', methods=['POST'])
-def purchase():
+@app.route('/start_order', methods=['POST'])
+def start_order():
     product_url = request.form.get('product_url', None)
     if product_url is None:
         abort(403)
 
-    return render_template('purchase_done.html', result=purchase_product(product_url))
-
-
-# this is where TwoTap asks us to confirm the purchase with the private token
-@app.route('/purchase_confirm', methods=['POST'])
-def purchase_confirm():
-    body = request.get_json()
-
-    purchase_id = body['purchase_id']
-
-    response = requests.post(ROOT_URL + '/v1.0/purchase/confirm',
-            params={'private_token': app.config['TT_PRIVATE_TOKEN']},
-            headers={'content-type': 'application/json'},
-            data=json.dumps({'purchase_id': purchase_id}))
-
-    log_response('Confirmed Purchase {}'.format(purchase_id), response.json())
-
-    return jsonify(response.json())
-
-
-# this is where TwoTap lets us know that a purchase is done; an alternative to polling for the result
-# this is NOT called when running requests with fake_confirm
-@app.route('/purchase_finished', methods=['POST'])
-def purchase_finished():
-    print 'PURCHASE FINISHED CALLED'
-    return ''
-
-
-# this is only an example of the TwoTap flow
-# in a real application this would probably be executed in an asynchronous task
-def purchase_product(product_url):
     # add the product to cart
     print "Adding '{}' to cart".format(product_url)
     response = requests.post(ROOT_URL + '/v1.0/cart',
             headers={'content-type': 'application/json'},
             params={'public_token': app.config['TT_PUBLIC_TOKEN'], 'test_mode': app.config['TT_TEST_MODE']},
-            data=json.dumps({'products': [product_url]}))
+            data=json.dumps({
+                'products': [product_url],
+                'finished_url': url_for('cart_finished', _external=True),
+                }))
 
-    log_response('Added to cart', response.json())
+    log_body('Added to cart', response.json())
 
-    cart_id = str(response.json()['cart_id'])
+    return 'ORDER STARTED'
 
-    # poll for the cart status
-    while response.json()['message'] == 'still_processing':
-        time.sleep(2)
-        response = requests.get(ROOT_URL + '/v1.0/cart/status',
-            params={'public_token': app.config['TT_PUBLIC_TOKEN'], 'cart_id': cart_id, 'test_mode': app.config['TT_TEST_MODE']})
 
-        log_response('Checking status for cart {}'.format(cart_id), response.json())
+# TT calls this URL when the products have been added to the cart
+@app.route('/cart_finished', methods=['POST'])
+def cart_finished():
+    body = request.get_json()
 
-    if response.json()['message'] != 'done':
-        return 'failed'
+    log_body('Cart finished', body)
 
-    fields_input = make_fields_input(response.json()['sites'])
+    cart_id = body['cart_id']
+    sites = body['sites']
+
+    fields_input = make_fields_input(sites)
 
     # trigger the purchase
     response = requests.post(ROOT_URL + '/v1.0/purchase',
@@ -90,7 +62,6 @@ def purchase_product(product_url):
             data=json.dumps({
                 'cart_id': cart_id,
                 'fields_input': fields_input,
-                'products': [product_url],
                 'confirm': {
                     'method': 'sms', # we want the user to confirm by sms
                     'phone': '5555555555',
@@ -101,18 +72,36 @@ def purchase_product(product_url):
 
     purchase_id = response.json()['purchase_id']
 
-    log_response('Triggered purchase {}'.format(purchase_id), response.json())
+    log_body('Triggered purchase {}'.format(purchase_id), response.json())
 
-    # poll for the purchase status
+    return ''
 
-    while response.json()['message'] == 'still_processing':
-        time.sleep(2)
-        response = requests.get(ROOT_URL + '/v1.0/purchase/status',
-            params={'public_token': app.config['TT_PUBLIC_TOKEN'], 'purchase_id': purchase_id, 'test_mode': app.config['TT_TEST_MODE']})
 
-        log_response('Checking status for purchase {}'.format(purchase_id), response.json())
+# this is where TwoTap asks us to confirm the purchase with the private token
+@app.route('/purchase_confirm', methods=['POST'])
+def purchase_confirm():
+    body = request.get_json()
 
-    return response.json()['message']
+    purchase_id = body['purchase_id']
+
+    log_body('Confirm purchase callback called for {}'.format(purchase_id), body)
+
+    response = requests.post(ROOT_URL + '/v1.0/purchase/confirm',
+            params={'private_token': app.config['TT_PRIVATE_TOKEN']},
+            headers={'content-type': 'application/json'},
+            data=json.dumps({'purchase_id': purchase_id}))
+
+    log_body('Confirmed Purchase {}'.format(purchase_id), response.json())
+
+    return jsonify(response.json())
+
+
+# this is where TwoTap lets us know that a purchase is done; an alternative to polling for the result
+# this is NOT called when running requests with fake_confirm
+@app.route('/purchase_finished', methods=['POST'])
+def purchase_finished():
+    print 'PURCHASE FINISHED CALLED'
+    return ''
 
 
 # dummy checkout data
@@ -160,9 +149,9 @@ def make_fields_input(cart_sites):
     return fields_input
 
 
-def log_response(message, response_body):
+def log_body(message, body):
     print ">>>>> {} >>>>>".format(message)
-    print json.dumps(response_body, indent=2)
+    print json.dumps(body, indent=2)
     print
 
 
